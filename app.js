1f52af80945a9012b9c570b336970f7bc6d61395
Tyler Keller <tylerkeller@mines.edu>
2023-08-31 14:20:42 -0600

    initialized firebase

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 4000;
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(express.static('public'));
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_d7DFaGaSLU1VyPNYeFruraAYqLXYS3o",
+  authDomain: "tylerkeller-dev.firebaseapp.com",
+  projectId: "tylerkeller-dev",
+  storageBucket: "tylerkeller-dev.appspot.com",
+  messagingSenderId: "808076940727",
+  appId: "1:808076940727:web:ff0c8243e71a3e81d28432",
+  measurementId: "G-16Z1BNN8ND"
+};
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

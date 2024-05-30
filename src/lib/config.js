1d57d40bd25d40fdff7e529d69146a8676d8a5ef
Tyler Keller <tylerkeller@mines.edu>
2024-05-30 01:13:24 -0400

    update

---
+++ src/lib/config.js
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'tyler keller'
+export const description = 'tyler keller\'s personal site. a place to share my thoughts and projects.'
+export const url = dev ? 'http://localhost:5173/' : 'https://tylerkeller.dev/'
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="flex center-text space ">
+
+<p class="">hi. i'm tyler.</p>
+
+</div>

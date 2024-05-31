a3e7cf6424cf4d5580d72cdad87d1f0af5a68bca
Tyler Keller <tylerkeller@mines.edu>
2024-05-30 21:36:08 -0400

    worked on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
-<script>
+<script lang="js">
 
 </script>
 
-<div class="flex center-text space ">
+<div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
 
-<p class="">hi. i'm tyler.</p>
+    <div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
+        <p class="">hi. i'm tyler.</p>
+        <div class="flex flex-row">
+            <p class="pr-1 pt-1">i'm a software engineer </p>
+            <p class="p-1 text-xs">(obv. not a designer)</p>
+            <p class="p-1"> -- w/ a focus in ML.</p>
+        </div>
+        <p class="pb-16">no stranger to shipping.</p>
+    </div>
 
+    <div class="flex justify-center pt-8">
+        <div class="mx-auto bg-black rounded-lg inline-flex flex-col items-center justify-center p-4">
+            <div class="text-center">
+                <p class="text-sm">"Anything which is not prohibited by the laws</p>
+                <p class="text-sm">of physics is possible given sufficient knowledge."</p>
+            </div>
+            <p class="text-xs mt-2">- Principle of Optimism</p>
+        </div>
+    </div>
 </div>

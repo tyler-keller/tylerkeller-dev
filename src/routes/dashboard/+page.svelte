d56f8553dac954aba200be43f4ef4c1ead7d197a
Tyler Keller <tylerkeller@mines.edu>
2024-09-01 23:40:28 -0600

    update

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class='mx-auto font-bold text-xl pb-8'>weekly review dashboard</h1>
+
+<div class='w-3/4 flex-row space-y-8'>
+    <h2 class='mx-auto'>T1D</h2>
+    <h2 class='mx-auto'>fitness</h2>
+    <h2 class='mx-auto'>nutrition</h2>
+    <h2 class='mx-auto'>cleanly room</h2>
+    <h2 class='mx-auto'>morning routine</h2>
+    <h2 class='mx-auto'>evening routine</h2>
+    <h2 class='mx-auto'>financial audit</h2>
+    <h2 class='mx-auto'>progress on projects</h2>
+</div>

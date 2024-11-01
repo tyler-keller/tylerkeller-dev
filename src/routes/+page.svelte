ff94131334e68de3c6919ebf5d2967394ea93302
Tyler Keller <tylerkeller@mines.edu>
2024-10-31 20:16:09 -0600

    states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,18 +8,38 @@
             <p class="p-1 text-center"> -- w/ a focus in ML.</p>
         </div>
         <!-- <p class="">no stranger to shipping.</p> -->
-        <div class='md:py-8'>
+        <div class='md:py-8 justify-center'>
             <p class="text-center pb-2">the journey so far:</p>
-            <div class="md:flex md:flex-row flex-cols text-center md:w-3/4 md:space-x-16 lg:space-y-0 sm:space-y-1">
-                <p class="">TX</p>
-                <p class="">HI</p>
-                <p class="">CA</p>
-                <p class="">NJ</p>
-                <p class="">DEU</p>
-                <p class="">VA</p>
-                <p class="">CO</p>
+            <div class="md:flex md:flex-row sm:flex-col content-center md:w-3/4 md:space-x-16 lg:space-y-0 md:space-y-16 sm:space-y-16 md:pt-4">
+                <a href='/states/texas' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">TX </p>
+                    <img src="./texas.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/hawaii' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">HI</p>
+                    <img src="./hawaii.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/california' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">CA</p>
+                    <img src="./california.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/new-jersey' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">NJ</p>
+                    <img src="./new-jersey.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/germany' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">DEU</p>
+                    <img src="./germany.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/virginia' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">VA</p>
+                    <img src="./virginia.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
+                <a href='/states/colorado' class="flex flex-row space-x-3 justify-center group hover:rotate-6">
+                    <p class="text-center group-hover:text-blue-600">CO</p>
+                    <img src="./colorado.png" alt="" class="md:size-5/6 sm:size-1/12 group-hover:scale-105 transition-transform duration-200">
+                </a>
             </div>
         </div>
     </div>
-
 </div>
---
+++ src/routes/states/colorado/+page.svelte
@@ -0,0 +1,8 @@
+
+<div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
+    <div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
+        <h1 class='dark:text-white text-black font-bold text-xl pb-8'>colorado</h1>
+        <h2 class='dark:text-white text-black'>second longest place i've lived, soon to overtake va.</h2>
+        <h2 class='dark:text-white text-black'>probably well i'll settle given the chance.</h2>
+    </div>
+</div>

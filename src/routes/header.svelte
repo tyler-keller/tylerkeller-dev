1c9ac1353f7a2f7c65b2fb16f543130a15cecd6a
tyler-keller <tylerkeller@mines.edu>
2025-01-04 03:42:20 +0000

    added projects

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -5,17 +5,17 @@
 <div class="mx-auto pt-10">
     <ul class="flex flex-row mx-auto space-x-8 lg:space-x-16">
         <li>
-            <a href="/" class='hover:underline'>home</a>
+            <a href="/" class='hover:underline'>tylerkeller.dev</a>
         </li>
-        <li>
+        <!-- <li>
             <a href="https://github.com/tyler-keller?tab=repositories" target="_blank" class='hover:underline'>projects</a>
-            <!-- <a href="/projects" class='hover:underline'>projects</a> -->
+            <a href="/projects" class='hover:underline'>projects</a>
         </li>
         <li>
             <a href="https://0xt4k.substack.com/" target="_blank" class='hover:underline'>posts</a>
         </li>
         <li>
             <a href="/recs" class='hover:underline'>recs</a>
-        </li>
+        </li> -->
     </ul>
 </div>
---
+++ src/routes/modal.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let isOpen = false; // Controls modal visibility
+    export let content = ""; // Dynamic content to display
+
+    const closeModal = () => {
+        isOpen = false;
+    };
+</script>
+
+{#if isOpen}
+    <div class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+        <div class="bg-white rounded-lg w-3/4 p-5 relative">
+            <button class="absolute top-2 right-2 text-red-500" on:click={closeModal}>Close</button>
+            <div class="overflow-auto max-h-[80vh]">
+                {@html content} <!-- Renders dynamic HTML content -->
+            </div>
+        </div>
+    </div>
+{/if}

2bcb4414dbccf574c9edd993fb73ebf499ebad7a
tyler-keller <tylerkeller@mines.edu>
2025-11-19 08:22:46 +0000

    i'm reverting back to olden times...

--- src/routes/rubiks-cube/+page.svelte
+++
@@ -1,5 +0,0 @@
-<div class="">
-
-    <p>This is work on the NN Rubiks Cube solver.</p>
-
-</div>
--- src/routes/states/new-jersey/+page.svelte
+++
@@ -1,8 +0,0 @@
-
-<div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
-    <div class="p-8 max-w-prose mx-auto space-y-8 flex-row">
-        <h1 class='dark:text-white text-black font-bold text-xl pb-8'>new jersey</h1>
-        <h2 class='dark:text-white text-black'>probably my least favorite state (no offense jerseyites).</h2>
-        <h2 class='dark:text-white text-black'>diagnosed w/ type one here.</h2>
-    </div>
-</div>
